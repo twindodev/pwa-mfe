c2b4e319dd855f92cf41b090c8df43bbaa1fe2d0
twindodev <97145513+twindodev@users.noreply.github.com>
2022-01-14 15:42:44 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -39,16 +39,16 @@ const coffees = [
 ];
 const showCoffees = () => {
   let output = "";
-  coffees.forEach(
-    ({ name, image }) =>
-      (output += `
-              <div class="card">
-                <img class="card--avatar" src=${image} />
-                <h1 class="card--title">${name}</h1>
-                <a class="card--link" href="#">Taste</a>
-              </div>
-              `)
-  );
+//   coffees.forEach(
+//     ({ name, image }) =>
+//       (output += `
+//               <div class="card">
+//                 <img class="card--avatar" src=${image} />
+//                 <h1 class="card--title">${name}</h1>
+//                 <a class="card--link" href="#">Taste</a>
+//               </div>
+//               `)
+//   );
   container.innerHTML = output;
 };
 
