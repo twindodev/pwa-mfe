2653adb78f6999f81a312758e899e4ad2d40f112
twindodev <97145513+twindodev@users.noreply.github.com>
2022-01-14 16:29:09 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -68,7 +68,7 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register("./serviceWorker.js")
+      .register("../serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   }
